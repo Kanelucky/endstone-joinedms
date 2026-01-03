3506835958ccb50f3b1a3793b2676b8b17391988
Kanelucky <tuankietdoan46@gmail.com>
2026-01-03 22:19:41 +0700

    Update joinedms.py

--- src/endstone_joinedms/joinedms.py
+++ src/endstone_joinedms/joinedms.py
@@ -7,16 +7,22 @@ class Joinedms(Plugin):
     api_version = "0.10"
     @event_handler
     def on_player_join(self, event: PlayerJoinEvent):
-        event.join_message = (ColorFormat.DARK_GREEN + "[+] " + ColorFormat.WHITE + f"{event.player.name}")
+        msg = self.config.get("joined-message")
+        msg = msg.replace("{player_name}", event.player.name)
+        event.join_message = (msg)
     @event_handler
     def on_player_quit(self, event:PlayerQuitEvent):
-        event.quit_message = (ColorFormat.DARK_RED + "[-] " + ColorFormat.WHITE + f"{event.player.name}")
+        msg = self.config.get("quit-message")
+        msg = msg.replace("{player_name}", event.player.name)
+        event.quit_message = (msg)
 
 
     def on_load(self) -> None:
-        self.logger.info("on_load is called!")
+        self.logger.info("Joinedms loaded")
+        self.save_default_config()
     def on_enable(self) -> None:
-        self.logger.info("on_enable is called!")
+        self.save_config()
+        self.logger.info(ColorFormat.DARK_GREEN + "JoinedMS enabled. Enjoy!")
         self.register_events(self)
     def on_disable(self) -> None:
-        self.logger.info("on_disable is called!")
+        self.logger.info("JoinedMS disabled. Goodbye!")
